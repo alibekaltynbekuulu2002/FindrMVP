6a23f7986e5155d37b2691c2f19e9950fbc2ac79
Alibek Altynbek uulu <alibekprogramming@gmail.com>
2023-03-13 00:10:24 +0600

    features-loginscreen-design

--- components/statusBars/FocusedStatusBar.tsx
+++ components/statusBars/FocusedStatusBar.tsx
@@ -1,10 +1,17 @@
 import { View, Text, StatusBar } from "react-native";
 import React from "react";
 import { useIsFocused } from "@react-navigation/native";
+import { COLORS } from "../../constants/theme";
 
 const FocusedStatusBar = () => {
   const isFocused = useIsFocused();
-  return isFocused ? <StatusBar animated={true} /> : null;
+  return isFocused ? (
+    <StatusBar
+      animated={true}
+      backgroundColor={COLORS.dark}
+      barStyle={"light-content"}
+    />
+  ) : null;
 };
 
 export default FocusedStatusBar;
